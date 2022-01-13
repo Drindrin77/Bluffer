f85e0d453660c77edfa91c976183cdce9c773f9e
nicolas <nicolasfau93@gmail.com>
2022-01-13 21:44:26 +0100

    [FIX] remove socket check

--- back/app/config/socket.js
+++ back/app/config/socket.js
@@ -5,27 +5,7 @@ module.exports = async (server) => {
     cors: { origin: "*" },
   });
 
-  io.on("connection", (socket) => {
-    console.log("A client try to join a room");
-    for (const client of io.sockets.sockets) {
-      console.log(socket.id);
-      console.log(client[0]);
-      if (
-        socket.id !== client[0] &&
-        socket.handshake.address === client[1].handshake.address
-      ) {
-        socket.emit("socketAlreadyExists", {
-          message: "user already connected",
-        });
-        return;
-      }
-    }
-    console.log("ok");
-    socket.emit("connectionEstablished", { message: "ok" });
+  io.services = {};
 
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
-    });
-  });
   return io;
 };
