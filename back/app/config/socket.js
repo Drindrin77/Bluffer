d86db047b71c7a8a0e0501025240838d54c4e130
nicolas <nicolasfau93@gmail.com>
2022-01-13 21:04:36 +0100

    [FIX] check client

--- back/app/config/socket.js
+++ back/app/config/socket.js
@@ -7,18 +7,15 @@ module.exports = async (server) => {
 
   io.on("connection", (socket) => {
     console.log("A client try to join a room");
-    const clients = io.sockets.client();
-    if (clients) {
-      for (const clientId of clients) {
-        const clientSocket = io.sockets.sockets.get(clientId);
-        if (socket.handshake.address === clientSocket.handshake.address) {
-          socket.emit("socketAlreadyExists", {
-            message: "user already connected",
-          });
-          return;
-        }
+    for (const client of io.sockets.sockets) {
+      if (socket.handshake.address === client[1].hashandke) {
+        socket.emit("socketAlreadyExists", {
+          message: "user already connected",
+        });
+        return;
       }
     }
+
     socket.emit("connectionEstablished", { message: "ok" });
 
     socket.on("disconnect", () => {
