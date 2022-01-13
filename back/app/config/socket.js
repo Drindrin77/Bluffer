e76959036d4b839d0dda165775f1bf91f934e4cf
nicolas <nicolasfau93@gmail.com>
2022-01-13 21:17:34 +0100

    [FIX] check user

--- back/app/config/socket.js
+++ back/app/config/socket.js
@@ -8,14 +8,14 @@ module.exports = async (server) => {
   io.on("connection", (socket) => {
     console.log("A client try to join a room");
     for (const client of io.sockets.sockets) {
-      if (socket.handshake.address === client[1].hashandke) {
+      if (socket.handshake.address === client[1].handshake.address) {
         socket.emit("socketAlreadyExists", {
           message: "user already connected",
         });
         return;
       }
     }
-
+    console.log("ok");
     socket.emit("connectionEstablished", { message: "ok" });
 
     socket.on("disconnect", () => {
